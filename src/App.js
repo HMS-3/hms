f8f3bbbb32b1fd5d9dab441375e11fff8a7aa842
Nithish <nithishgowda98@gmail.com>
2023-05-25 12:32:43 +0530

    push changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>hiii nithish gowda</h1>
+      <h1>hiii nithish gowda successfully launched
+        
+      </h1>
     </div>
   );
 }
