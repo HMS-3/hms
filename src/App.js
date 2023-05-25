a546383e4f955460d4f5ba81868f32fdb600c683
2amlover <www.insultu@gmail.com>
2023-05-25 12:38:11 +0530

    mohan

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>hiii nithish gowda successfully launched
+      <h1>hiii mohan successfully launched
         
       </h1>
     </div>
